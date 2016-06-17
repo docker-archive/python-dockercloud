9f484a8a59a044cccd58d05a091cfb260c4d3ef3
tifayuki <tifayuki@gmail.com>
2016-06-17 17:58:30 +0200

    remove namespace from action endpoints

--- dockercloud/api/action.py
+++ dockercloud/api/action.py
@@ -6,6 +6,7 @@ from .base import Immutable, StreamingLog
 class Action(Immutable):
     subsystem = 'audit'
     endpoint = "/action"
+    namespaced = False
 
     @classmethod
     def _pk_key(cls):
